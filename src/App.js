69ddb1204a69a6c20d81369239279aceab723f36
salsifitewn <57296389+salsifitewn@users.noreply.github.com>
2023-11-05 18:33:26 +0000

    add image form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [imageUrl, setImageUrl] = React.useState('https://i.imgur.com/7yPH8b2.png');
+  const handleChange = (event) => {
+    setImageUrl(event.target.value);
+  }
+  
+  return (
+    <div className="App">
+      <h1>Computer Vision</h1>
+      <label>Image URL</label>
+      <input onChange={handleChange} type='text' placeholder='Enter Image URL' value={imageUrl}/>
+      // add two buttons with button type analyze and generate
+      <button type='button'>Analyze</button>
+      <button type='button'>Generate</button>
+    </div>
+  );
 }
 
 export default App;
