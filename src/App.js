dc925947b86c879e8e97bfb75d3e544003042bd9
salsifitewn <57296389+salsifitewn@users.noreply.github.com>
2023-11-05 18:51:48 +0000

    update to react 18

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import { useState } from "react";
 
 function App() {
-  const [imageUrl, setImageUrl] = React.useState('https://i.imgur.com/7yPH8b2.png');
+  const [imageUrl, setImageUrl] = useState('https://i.imgur.com/7yPH8b2.png');
   const handleChange = (event) => {
     setImageUrl(event.target.value);
   }
-  
+
   return (
     <div className="App">
       <h1>Computer Vision</h1>
-      <label>Image URL</label>
-      <input onChange={handleChange} type='text' placeholder='Enter Image URL' value={imageUrl}/>
-      // add two buttons with button type analyze and generate
-      <button type='button'>Analyze</button>
-      <button type='button'>Generate</button>
+      <label>Image imageUrl
+        <input onChange={handleChange} type='text' placeholder='Enter Image URL' value={imageUrl} />
+      </label>
+      <div>
+        <button type='button'>Analyze</button>
+        <button type='button'>Generate</button>
+      </div>
     </div>
   );
 }
