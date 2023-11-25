69bd06882082dbdbf939e63a4d3d9e03c739f553
salsifitewn <57296389+salsifitewn@users.noreply.github.com>
2023-11-25 16:14:22 +0000

    Add Azure image analysis functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { useState } from "react";
-
+import analyzeImage from './azure-image-analysis';
 function App() {
   const [imageUrl, setImageUrl] = useState('https://i.imgur.com/7yPH8b2.png');
   const handleChange = (event) => {
     setImageUrl(event.target.value);
   }
-
+  const [isAnalyzing, setIsAnalyzing] = useState(false);
+  const handleAnalyze = async () => {
+    setIsAnalyzing(true);
+    const result = await analyzeImage(imageUrl);
+    console.log(result);
+    setIsAnalyzing(false);
+  }
   return (
     <div className="App">
       <h1>Computer Vision</h1>
@@ -14,8 +20,9 @@ function App() {
         <input onChange={handleChange} type='text' placeholder='Enter Image URL' value={imageUrl} />
       </label>
       <div>
-        <button type='button'>Analyze</button>
+        <button type='button' onClick={handleAnalyze}>Analyze</button>
         <button type='button'>Generate</button>
+        {isAnalyzing && <p>Analysing...</p>}
       </div>
     </div>
   );
